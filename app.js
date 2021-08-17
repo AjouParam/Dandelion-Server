27cbe06d497448837c024710651c89369ec012e3
julie0005 <julie0005@ajou.ac.kr>
2021-08-17 21:12:16 +0900

    npm module added

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const morgan=require('morgan');
 const cors=require('cors');
 const session=require("express-session");
 const router=require("./src/routes");
+const MongoStore = require('connect-mongo')(session);
 
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded( {extended : false } ));
 app.use(cors());
 app.use("/", router);
-
+/*
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+      url: "mongodb://localhost/test",
+      collection: "sessions"
+    })
+  }));
+*/
 module.exports=app;
