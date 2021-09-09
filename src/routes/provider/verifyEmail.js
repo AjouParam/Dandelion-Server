45753743bb02ca2129fdeed3ec1421d9a23d5f59
coreight98 <maxcha98@naver.com>
2021-09-09 17:20:54 +0900

    refactor : modify send email format

--- src/routes/provider/verifyEmail.js
+++ src/routes/provider/verifyEmail.js
@@ -20,6 +20,12 @@ module.exports = Verify = {
       to: param.toEmail, // 수신할 이메일
       subject: param.subject, // 메일 제목
       text: param.text, // 메일 내용
+      html: `<h1 style="color: #5e9ca0;">안녕하세요! <span style="color: #2b2301;">test_Jang님</span> 민들레 입니다!</h1>
+      <h2 >추억을 기록하고 공유하는 소셜 네트워크 서비스 <span style="color: #5e9ca0;">민들레</span></h2>
+      <p>비밀번호 변경을 위한 인증코드 입니다.<br />절대 다른 사람에게 귀하의 인증 코드를 공개하지 마십시오.</p>
+      <p><h2 style="color: #2e6c80;">인증 코드</h2>
+      <span style="background-color: #2b2301; color: #fff; display: inline-block; padding: 3px 10px; font-weight: bold; border-radius: 5px;">
+      ${param.text}</span> 입니다.`,
     };
     // 메일 발송
     transporter.sendMail(mailOptions, function (error, info) {
