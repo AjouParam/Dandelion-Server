4fef937c06bfcd46453df763403b16340c0e5c1c
Team1 <38166372+Coreight98@users.noreply.github.com>
2021-10-10 10:21:18 +0900

    feat : add event create api

--- src/routes/controller/eventControl.js
+++ src/routes/controller/eventControl.js
@@ -1,9 +1,39 @@
 const Dandelion = require('../../models/Dandelion');
 const { resultResponse, basicResponse } = require('../../config/response');
 const Event = require('../../models/Event');
+const { checkNotExist, checkPost } = require('./Validation/Dandelion');
+const { getKoreanTime } = require('../provider/util');
 
 const event = {
-  create: async (req, res) => {},
+  create: async (req, res) => {
+    const userId = req.decoded._id;
+    const dandelionId = req.params.dandelionId;
+    const { title, text, location, images, rewards, firstComeNum } = req.body;
+    const isDandelionNotExist = await checkNotExist(dandelionId);
+    if (isDandelionNotExist) return res.json(basicResponse('해당 민들레가 존재하지 않습니다.', false));
+    const newEvent = new Event({
+      _user: userId,
+      _dandelion: dandelionId,
+      location: {
+        type: 'Point',
+        coordinates: [location.longitude, location.latitude],
+      },
+      firstComeNum,
+      title,
+      text,
+      rewards,
+      images: images,
+      createdAt: await getKoreanTime(),
+      startDate: await getKoreanTime(),
+    });
+    newEvent
+      .save()
+      .then((result) => res.json(resultResponse('이벤트를 생성하였습니다.', true, { data: result })))
+      .catch((err) => {
+        console.log(err);
+        return res.json(basicResponse('이벤 트 생성 중 에러가 발생하였습니다.'));
+      });
+  },
   delete: async (req, res) => {},
   get: async (req, res) => {},
   update: async (req, res) => {},
