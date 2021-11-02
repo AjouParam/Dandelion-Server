b8624f1c9fcf3268adc1e5df6b67da4c578113c3
julie0005 <julie0005@ajou.ac.kr>
2021-11-02 18:56:27 +0900

    fix : package setting

--- src/routes/controller/mypageControl.js
+++ src/routes/controller/mypageControl.js
@@ -14,7 +14,75 @@ const { getKoreanTime } = require('../provider/util');
 const mongoose = require('mongoose');
 
 const myPage = {
-  getMyPost: async (req, res) => {},
+  getMyPost: async (req, res) => {
+    // 사용자 게시글 불러오기 권한 validation 확인
+    // 사용자 자체 validation 확인
+    const userId = req.decoded._id;
+    const page = parseInt(req.query.page);
+    const maxPost = parseInt(req.query.maxPost);
+    const hidePost = page === 1 ? 0 : (page - 1) * maxPost;
+
+    if (!page || !maxPost) return res.json(basicResponse('페이지와 관련된 query parameter가 누락되었습니다.'));
+
+    Post.aggregate([
+      { $match: { _user: mongoose.Types.ObjectId(userId) } },
+      {
+        $lookup: {
+          from: 'users',
+          localField: '_user',
+          foreignField: '_id',
+          as: '_user',
+        },
+      },
+      { $unwind: '$_user' },
+      { $sort: { createdAt: -1 } },
+      { $skip: hidePost },
+      { $limit: maxPost },
+      {
+        $lookup: {
+          from: 'comments',
+          localField: '_id',
+          foreignField: '_post',
+          as: 'comments',
+        },
+      },
+      {
+        $lookup: {
+          from: 'likes',
+          localField: '_id',
+          foreignField: '_post',
+          as: 'likes',
+        },
+      },
+      {
+        $project: {
+          location: {
+            longitude: { $arrayElemAt: ['$location.coordinates', 0] },
+            latitude: { $arrayElemAt: ['$location.coordinates', 1] },
+          },
+          createdAt: 1,
+          updatedAt: 1,
+          title: 1,
+          text: 1,
+          images: 1,
+          _dandelion: 1,
+          '_user._id': 1,
+          '_user.name': 1,
+          '_user.thumbnail': 1,
+          comments: { $size: '$comments' },
+          likes: { $size: '$likes' },
+          isEvent: 1,
+        },
+      },
+    ])
+      .then((result) => {
+        return res.json(resultResponse('내가 작성한 게시글입니다.', true, { data: result }));
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.json(basicResponse('내가 작성한 게시글을 가져오는 중 에러가 발생하였습니다.'));
+      });
+  },
   getMyDandelion: async (req, res) => {},
 };
 
